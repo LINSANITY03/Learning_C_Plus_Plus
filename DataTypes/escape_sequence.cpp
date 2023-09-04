f8a283041b4272b95879e9a7a2f165c29379036c
pujan tamang <pujantamang92@gmail.com>
2023-09-04 15:18:06 +0100

    added escape-sequence

---
+++ DataTypes/escape_sequence.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "hello\"hehehe\"world"; // \t - enter a tab character
+    // \n - new line
+    // \v - vertical tab
+    // \0 - denotes the end of string
+    // \" - indicates a quote
+    // \\ - inform the compiler to print one backslash
+    return 0;
+}
