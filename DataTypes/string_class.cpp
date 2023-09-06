5467936877686fab26659764679245108009a627
pujan tamang <pujantamang92@gmail.com>
2023-09-06 21:24:49 +0100

    properties of string

--- DataTypes/string_class.cpp
+++ DataTypes/string_class.cpp
@@ -14,5 +14,17 @@ int main()
     std::string in_greeting;
     std::getline(std::cin, in_greeting); // getline for getting all input strings
     std::cout << in_greeting << std::endl;
+
+    std::string username = "Pujan";
+    username += " Thing";
+    username.append(". Welcome to programming!");
+    username.insert(3, "          "); // first is the index before inserting
+    username.erase(1, 2);             // index, total removal elements
+    // or we could pass one number to determine removal after the index
+    username.pop_back();               // same as pop(-1) or pop() in python
+    username.replace(0, 3, "okxa ta"); // (start, letter after start, replace_word)
+    std::cout
+        << username << std::endl;
+
     return 0;
 }
